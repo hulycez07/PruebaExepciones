c2a6e00b0aa618c39cb1178699ee985a8eff73d1
OscarUlises <OscarUlises@Ulises.gateway.2wire.net>
2013-06-13 21:46:59 -0600

    prueba exepciones commit

---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	ListaNumeros ln=new ListaNumeros();
+		//ln.imprimirLista();
+		
+		ArchivoDemo ad=new ArchivoDemo();
+		ad.AnalizarRuta("C:\\Users\\hulycez07\\Documents\\ejemplo.txt");
+	}
+
+}
